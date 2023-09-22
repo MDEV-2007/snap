00c3f3f802bfba6487fb1a1eec4894668f884d8b
Abdurazzoq <rayyan.dev@mail.ru>
2023-09-22 14:21:27 +0500

    awdawd

--- api/views.py
+++ api/views.py
@@ -5,9 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
-
+from .models import User
 from .serializers import UserSerializer
 
 
