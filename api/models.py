134ec557f31e0d0cab01ef46f4d2d2738e4b36bf
MDEV-2007 <uzbekcoders0706@gmail.com>
2023-09-24 20:23:14 +0500

    murodulla

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatar/')
+    avatar = models.ImageField(upload_to='avatar/', default='../static/images/default.jpg')
     
     def __str__(self):
         return self.username
