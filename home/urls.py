d277e0c9f29e484f013e7891aba400d44e494fdc
MDEV-2007 <uzbekcoders0706@gmail.com>
2023-10-02 13:49:26 +0500

    murodulla

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='home'),
+    path('explore/', explore, name='explore'),
+    path('search/', search, name='search'),
+    path('notifications/', notification, name='notification'),
+    path('reels/', reels, name='reels'),
+    path('create/', create, name='create'),
+    path('messages/', message, name='messages'),
+    path('profiles/', profile, name='profile'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'screen/index.html')
+
+
+def explore(request):
+    return render(request, 'screen/explore.html')
+
+
+def search(request):
+    return render(request, 'screen/search.html')
+
+
+def notification(request):
+    return render(request, 'screen/notification.html')
+
+
+def reels(request):
+    return render(request, 'screen/reels.html')
+
+def create(request):
+    return render(request, 'screen/create.html')
+
+
+def message(request):
+    return render(request, 'screen/message.html')
+
+
+def profile(request):
+    return render(request, 'screen/profile.html')
